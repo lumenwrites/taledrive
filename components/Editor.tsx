9562e42e8a556b50dfaad0d116adfffb166a2b13
Ray Alez <raymestalez@gmail.com>
2021-05-31 15:51:38 +0100

    Update.

---
+++ components/Editor.tsx
@@ -0,0 +1,44 @@
+// Import React dependencies.
+import { useEffect, useMemo, useState } from 'react'
+// Import the Slate editor factory.
+import { createEditor } from 'slate'
+// Import the Slate components and React plugin.
+import { Slate, Editable, withReact } from 'slate-react'
+
+// TypeScript Users only add this code
+import { BaseEditor } from 'slate'
+import { ReactEditor } from 'slate-react'
+
+type CustomElement = { type: 'paragraph'; children: CustomText[] }
+type CustomText = { text: string }
+
+declare module 'slate' {
+  interface CustomTypes {
+    Editor: BaseEditor & ReactEditor
+    Element: CustomElement
+    Text: CustomText
+  }
+}
+
+const Editor = () => {
+  const editor = useMemo(() => withReact(createEditor()), [])
+  // Add the initial value when setting up our state.
+  const [value, setValue] = useState([
+    {
+      type: 'paragraph',
+      children: [{ text: 'A line of text in a paragraph.' }],
+    },
+  ])
+  
+  return (
+    <Slate
+      editor={editor}
+      value={value}
+      onChange={newValue => setValue(newValue)}
+    >
+      <Editable />
+    </Slate>
+  )
+}
+
+export default Editor
